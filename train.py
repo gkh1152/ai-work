29540510608198fb75fcd37bb0f0c340a31466d3
ghk1152 <mhgong@zjut.edu.cn>
2024-12-12 13:26:30 +0800

    run22

--- train.py
+++ train.py
@@ -4,6 +4,6 @@ os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
  
 if __name__ == '__main__':
     model = YOLO(model='ultralytics/cfg/models/11/yolo11.yaml')
-    # model.load('yolov8n.pt')
-    model.train(data='./data.yaml', epochs=2, batch=1, device='0', imgsz=640, workers=2, cache=False,
+    model.load('yolo11n.pt')
+    model.train(data='./data.yaml', epochs=32, batch=32, device='0', imgsz=640, workers=2, cache=False,
                 amp=True, mosaic=False, project='.output', name='exp')
